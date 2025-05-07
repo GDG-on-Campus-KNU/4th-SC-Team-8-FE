34e6ae101c92755f77af41769c34e2586ed05566
Moderator11 <p7925221@gmail.com>
2025-05-07 14:59:30 +0900

    MDFY: 웹캠 좌우반전 삭제

--- src/pages/game/utils/drawCanvas.ts
+++ src/pages/game/utils/drawCanvas.ts
@@ -8,8 +8,8 @@ export const drawCanvas = (ctx: CanvasRenderingContext2D, results: Results) => {
   ctx.save();
   ctx.clearRect(0, 0, width, height);
   // canvas의 좌우 반전
-  ctx.scale(-1, 1);
-  ctx.translate(-width, 0);
+  // ctx.scale(-1, 1);
+  // ctx.translate(-width, 0);
   // capture image 그리기
   ctx.drawImage(results.image, 0, 0, width, height);
   // 손의 묘사
